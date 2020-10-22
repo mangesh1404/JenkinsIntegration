74d5bf0f928ca87ae69ea9c410fe48f76679ebd8
mangesh1404 <45553084+mangesh1404@users.noreply.github.com>
2020-10-22 08:39:37 +0530

    update test02

--- src/test/java/com/jbk/A_Test.java
+++ src/test/java/com/jbk/A_Test.java
@@ -2,6 +2,7 @@ package com.jbk;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -21,5 +22,12 @@ public class A_Test {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		Assert.assertEquals(driver.getTitle(), "JavaByKiran | Log in");
 
+	}
+	
+	@Test
+	public void test02() {
+		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("//button")).click();
 	}
 }
